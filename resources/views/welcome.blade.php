f7b9e8797a70cc7de282e4118171aa347089c034
jerrysanguyo <jsanguyo1624@gmail.com>
2024-11-03 14:57:56 +0800

    done with responsiveness

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,18 +18,22 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/welcome.css'])
 </head>
 <body>
-    <div class="row mt-5">
-        <div class="d-flex justify-content-center">
-            <img id="logo" src="{{ asset('image/IT.webp') }}" alt="Touch ID Icon">
-            <img id="logo" src="{{ asset('image/city.webp') }}" alt="Touch ID Icon">
-            <img id="logo" src="{{ asset('image/cho.webp') }}" alt="Touch ID Icon">
-        </div>
+    <div class="row mt-3">
+       <div class="col-lg-4 col-md-4 col-4 d-flex justify-content-center">
+           <img id="logo" src="{{ asset('image/IT.webp') }}" alt="IT logo">
+       </div>
+       <div class="col-lg-4 col-md-4 col-4 d-flex justify-content-center">
+           <img id="logo" src="{{ asset('image/city.webp') }}" alt="City logo">
+       </div>
+       <div class="col-lg-4 col-md-4 col-4 d-flex justify-content-center">
+           <img id="logo" src="{{ asset('image/cho.webp') }}" alt="CHO logo">
+       </div>
     </div>
     <div class="container-fluid">
         <div class="responsive-height text-center">
             <h1 id="glow"><strong>MIND YOUR<br>WELL-BEING AT WORK</strong></h1>
             <a href="{{ route('main.page') }}">
-                <img src="{{ asset('image/touch_id.webp') }}" alt="Touch ID Icon">
+                <img id="touch" src="{{ asset('image/touch_id.webp') }}" alt="Touch ID Icon">
             </a>
         </div>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/css/page.css',
+                'resources/css/assessment.css',
+                'resources/css/login-reg.css',
+                'resources/css/main.css',
+                'resources/css/welcome.css',
             ],
             refresh: true,
         }),
